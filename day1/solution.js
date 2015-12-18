80ee427a806c6ff591deaa27ce6847863979fd5f
Mark Simulacrum <mark.simulacrum@gmail.com>
2015-12-18 15:47:18 -0700

    Optimize solution for day 1.
     - Only split input once.
     - Don't filter input twice since we know that all characters
       are either '(' or ')'

--- day1/solution.js
+++ day1/solution.js
@@ -1,14 +1,15 @@
 let data = require("fs").readFileSync("./data").toString();
 
-let floorsUp = data.split("").filter(char => char === "(").length;
-let floorsDown = data.split("").filter(char => char === ")").length;
+let chars = data.split("");
+
+let floorsUp = chars.filter(char => char === "(").length;
+let floorsDown = chars.length - floorsUp;
 
 console.log("Ended up on:", floorsUp - floorsDown);
 
 // Part 2
 
 let floor = 0;
-let chars = data.split("");
 while (floor >= 0) chars.shift() === "(" ? floor++ : floor--;
 
 console.log("Went basement:", data.length - chars.length);
