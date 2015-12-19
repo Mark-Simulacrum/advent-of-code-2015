2c26b27c72b7d01694d32d42398995a0974db869
Mark Simulacrum <mark.simulacrum@gmail.com>
2015-12-18 17:22:11 -0700

    Remove mapping of arbitrary lengths to l/w/h.
     - No need
     - Unknown which is which anyway

--- day2/solution.js
+++ day2/solution.js
@@ -6,29 +6,22 @@ let presentDimensions = input
 	.split("\n")
 	.map(present =>
 		present.split("x")
-		.map(dimension => parseInt(dimension, 10))
-		.sort((a, b) => a - b)
-	)
-	.map(present => {
-		return {
-			l: present[0],
-			w: present[1],
-			h: present[2]
-		};
-	});
+			.map(dimension => parseInt(dimension, 10))
+			.sort((a, b) => a - b)
+	);
 
 let wrappingNeeded = util.sum(presentDimensions.map(present =>
-	present.l * present.w + // Area of smallest side
-	2 * present.l * present.w +
-	2 * present.w * present.h +
-	2 * present.h * present.l
+	present[0] * present[1] + // Area of smallest side
+	2 * present[0] * present[1] +
+	2 * present[1] * present[2] +
+	2 * present[2] * present[0]
 ));
 
 console.log("Sq. Ft. Wrapping Paper Needed:", wrappingNeeded);
 
 let ribbonNeeded = util.sum(presentDimensions.map(present =>
-	2 * present.l + 2 * present.w + // perimeter of smallest face, ribbon wrap
-	present.l * present.w * present.h // cubic area, bow
+	2 * present[0] + 2 * present[1] + // perimeter of smallest face, ribbon wrap
+	present[0] * present[1] * present[2] // cubic area, bow
 ));
 
 console.log("Ft. Ribbon Needed:", ribbonNeeded);
