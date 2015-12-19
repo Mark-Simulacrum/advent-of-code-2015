45d6696483848b419002805a90f929ac33ce3e82
Mark Simulacrum <mark.simulacrum@gmail.com>
2015-12-18 18:31:10 -0700

    Rework day 9 solution to use util.permutations.

--- day9/solution.js
+++ day9/solution.js
@@ -1,14 +1,17 @@
 let fs = require("fs");
-let without = require("lodash.without");
+let _ = require("lodash");
+let util = require("../util");
 
 let data = fs.readFileSync("./data").toString();
 
 let distances = {};
 
 data.split("\n").forEach(line => {
-	let fromCity = line.split(" to ")[0];
-	let toCity = line.split(" to ")[1].split(" = ")[0];
-	let distance = parseInt(line.split(" to ")[1].split(" = ")[1]);
+	let parts = line.split(" to ");
+
+	let fromCity = parts[0];
+	let toCity = parts[1].split(" = ")[0];
+	let distance = parseInt(parts[1].split(" = ")[1]);
 
 	if (!distances[fromCity]) distances[fromCity] = {};
 	if (!distances[toCity]) distances[toCity] = {};
@@ -22,39 +25,9 @@ function getDistance(cityA, cityB) {
 }
 
 const cities = Object.keys(distances);
-const cityAmount = Object.keys(distances).length;
 
-let cityPatterns = [];
-
-function generatePossibilities(array) {
-	if (array.length === 1) return array;
-
-	let poss = [];
-
-	for (let i = 0; i < array.length; i++) {
-		let value = array[i];
-
-		let otherValues = array.slice(0);
-		otherValues.splice(i, 1);
-
-		let possibilities = generatePossibilities(otherValues);
-
-		for (let x = 0; x < possibilities.length; x++) {
-			let possibility = possibilities[x];
-
-			poss.push([value].concat(possibility));
-		}
-	}
-
-	return poss;
-}
-
-let poss = generatePossibilities(cities);
-
-poss = poss.map(possibility => {
+let travelDistances = util.permutations(cities).map(possibility => {
 	let distanceTraveled = 0;
-
-	let cities = possibility.slice(0).join(" -> ");
 
 	let lastCity = possibility.shift();
 
@@ -64,7 +37,11 @@ poss = poss.map(possibility => {
 		lastCity = currentCity;
 	}
 
-	return { cities, distanceTraveled };
+	return distanceTraveled;
 });
 
-console.log(poss);
+let sortedDistances = travelDistances.sort((a, b) => a - b);
+
+console.log("Shortest distance (Part 1):", sortedDistances[0]);
+console.log("Longest distance (Part 2):",
+	sortedDistances[sortedDistances.length - 1]);
